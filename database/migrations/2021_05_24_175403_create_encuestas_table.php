2169494b69ee77b0e7d4f65b5a8b44d15da88bea
Esteban Saavedra <saavedra.esteban.1995@gmail.com>
2021-05-24 15:36:37 -0300

    hola

---
+++ database/migrations/2021_05_24_175403_create_encuestas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEncuestasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('encuestas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //relacion uno a uno con reservas
+            $table->unsignedBigInteger('reserva_id')->unique();
+            $table->foreign('reserva_id')->references('id')->on('reservas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encuestas');
+    }
+}
