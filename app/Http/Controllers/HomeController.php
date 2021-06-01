01a5da23129c1879c0f0121720f595640639025b
mauri995 <mauricio.cardenas1@outlook.com>
2021-06-01 15:36:01 -0300

    intento de side Bar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,12 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
+    public function __invoke()
     {
         return view('home');
     }
+
 }
